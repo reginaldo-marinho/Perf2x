857f556aa8b488ca565cb12c1a0c6090e7cc84aa
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2021-11-30 11:44:24 -0300

    commit

--- src/app/shared/mensagem-box/mensagem-box.component.ts
+++ src/app/shared/mensagem-box/mensagem-box.component.ts
@@ -9,13 +9,13 @@ import { ServiceMensagensInsert } from "./mensagem.service"
 export class MensagemBoxComponent {
     
     constructor(private mensagem: ServiceMensagensInsert){}
-
+    
     Mensagem?:string = "";
     OpenMessageBox(){
+        this.Mensagem = this.mensagem.CriarMensagemSucesso();
         var msg = document.getElementById("messagebox");
         msg?.classList.remove("message-box-end");
         msg?.classList.add("message-box");
-        this.Mensagem = this.mensagem.CriarMensagemSucesso();
    }
 
     CloseMessageBox(){

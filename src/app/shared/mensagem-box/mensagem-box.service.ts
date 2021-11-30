4f8d7bc71bb66a3998fde6819b1bebfb7e50bae2
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2021-11-29 23:15:25 -0300

    commit

--- src/app/shared/mensagem-box/mensagem-box.service.ts
+++ src/app/shared/mensagem-box/mensagem-box.service.ts
@@ -1,30 +1,45 @@
+export interface Mensagem{
+  CriarMensagem():string;
+}
+export class NiveisMensagem{
+  protected ConstSucesso:string = "Sucesso";
+  protected ConstAviso:string = "Aviso";
+  protected ConstErro: string = "Erro";
+}
 
+export class MensagensCRUD extends NiveisMensagem{
+  protected ConstFraseSucesso: string = "o registro foi salvo!";
+  protected ConstFraseAviso: string = "registro pode não ter sido salvo corretamente";
+  protected ConstFraseErro: string = "não foi possivel inserir o registro";
+}
 
-
+export class MensagemSucesso extends MensagensCRUD implements Mensagem{
+  CriarMensagem(){
+    return this.ConstSucesso.concat(" - ", this.ConstFraseSucesso);
+  }
+}
+export class MensagemAviso extends MensagensCRUD implements Mensagem{
+  CriarMensagem(){
+    return this.ConstAviso.concat(" - ", this.ConstFraseAviso);
+  }
+}
+export class MensagemErro extends MensagensCRUD implements Mensagem{
+  CriarMensagem(){
+    return this.ConstErro.concat(" - ", this.ConstFraseErro);
+  }
+}
 
 export class MensagemBoxService{
-    CloseMessageBox(){
-      
-        var msg = document.getElementById("messagebox");
-        if(msg)
-          for(let i=0; i < msg.classList.length;i++){
-            if(msg.classList[i]=="message-box"){
-              msg.classList.remove("message-box");
-              msg.classList.add("message-box-end");
-              break;
-            }  
-          }
+
+      CloseMessageBox(){
+        var msg = document.getElementById("messagebox")
+        msg?.classList.remove("message-box");
+        msg?.classList.add("message-box-end");
       }
   
       OpenMessageBox(){
         var msg = document.getElementById("messagebox");
-        if(msg)
-          for(let i=0; i < msg.classList.length;i++){
-            if(msg.classList[i]=="message-box-end"){
-              msg.classList.remove("message-box-end");
-              msg.classList.add("message-box");
-              break;
-          }   
-        }
+        msg?.classList.remove("message-box-end");
+        msg?.classList.add("message-box");
       }
 }

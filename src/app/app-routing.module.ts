b294d121a82baab10f71b8d8d414f70fefb12cb5
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-01-03 14:43:26 -0300

    commi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FormConteudoComponent } from './form-conteudo/form-conteudo.component';
 import { ListConteudoComponent } from './list-conteudo/list-conteudo.component';
 import { MenuBrazukasComponent } from './core/menu-conteudo/menu-brazukas.component';
 import { Page404Component } from './core/page-404/page-404.component';
+import { TemaEnsinoComponent } from './Tema-ensino/tema-ensino.component';
 
 const routes: Routes = [
   {path:'menu-brazukas', component: MenuBrazukasComponent},
   {path:'Form-conteudo', component: FormConteudoComponent},
   {path:'Form-conteudo/:codconteudo', component: FormConteudoComponent},
   {path:'ulti-posts'   , component: ListConteudoComponent},
+  {path:'tema-ensino'   , component: TemaEnsinoComponent},
   {path:'',redirectTo:'menu-brazukas', pathMatch: 'full'},
   {path:'**', component:Page404Component}
 ];

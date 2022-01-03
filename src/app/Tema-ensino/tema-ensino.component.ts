270bb9b45f46b6e0a021e58455a914378d44ed20
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-01-03 14:39:56 -0300

    commit

---
+++ src/app/Tema-ensino/tema-ensino.component.ts
@@ -0,0 +1,9 @@
+import {  Component} from "@angular/core";
+@Component({
+    templateUrl:'./tema-ensino.component.html',
+    styleUrls:['./tema-ensino.component.css']
+})
+export class TemaEnsinoComponent 
+{
+
+}

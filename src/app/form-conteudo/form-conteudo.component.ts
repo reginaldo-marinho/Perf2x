3185105c3becea17b27b65ecf2169148941b906e
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-01-05 14:46:06 -0300

    commit

--- src/app/form-conteudo/form-conteudo.component.ts
+++ src/app/form-conteudo/form-conteudo.component.ts
@@ -4,13 +4,14 @@ import { ActivatedRoute } from "@angular/router";
 import { LoaderService } from "../shared/Loader/loader.service";
 import { ConteudoDatalhes, ConteudoHeader } from "./conteudo-header";
 import { ConteudoService } from "./conteudo.service";
+import { ElementoConteudoService } from "./elemento-conteudo.service";
 @Component({
     templateUrl:'./form-conteudo.component.html',
     styleUrls:['./form-conteudo.component.css']
 })
 export class FormConteudoComponent implements OnInit,AfterViewInit{
 
-  constructor(private conteudoService: ConteudoService, private activatedRoute: ActivatedRoute, private fb: FormBuilder, private louder: LoaderService){  }
+  constructor(private conteudoService: ConteudoService, private activatedRoute: ActivatedRoute, private fb: FormBuilder, private louder: LoaderService, private HTMLDinamico: ElementoConteudoService){  }
   ngOnInit(){
 
       if (this.activatedRoute.snapshot.paramMap.get("codconteudo") != null){
@@ -19,7 +20,7 @@ export class FormConteudoComponent implements OnInit,AfterViewInit{
       }
   }
   ngAfterViewInit(){
-    this.CriarElementoDetalheTipoTextArea()  
+    this.HTMLDinamico.CriarElementoDetalheTipoTextArea()  
   }
 
   AlterarConteudo   : boolean = false;
@@ -59,131 +60,21 @@ export class FormConteudoComponent implements OnInit,AfterViewInit{
     })
     this.conteudoDatalhesForm.push(DetalheGroup!);
   }
+  
   CriarElementoTexto(){
-    this.CriarElementoDetalheTipoTextArea();
+    this.HTMLDinamico.CriarElementoDetalheTipoTextArea();
     this.MapearTextAreaTextoVazioFormGroup();
   }
 
   CriarElementoImagem(){
-    this.CriarComponenteInputImagem();
+    this.HTMLDinamico.CriarComponenteInputImagem();
     this.MapearInputImagemVazioFormGroup();
   }
 
-  CriarDivInputDetalhes() :Node{
-      let  DivInputDetalhes = document.createElement("div");
-      DivInputDetalhes.setAttribute("class","mb-3 me-1 ms-1");
-      return DivInputDetalhes;
-  }
-  
-  CriarDivInputImagem() :Node{
-    let  DivInputDetalhes = document.createElement("div");
-    DivInputDetalhes.setAttribute("class","mb-3 me-1 ms-1");
-    DivInputDetalhes.setAttribute("id",String(document.getElementsByTagName("img").length+1))
-    return DivInputDetalhes;
-  }
-
-  CriarElementoDetalheTipoTextArea(){
-    let AreaDeConteudo = document.getElementById("area-de-conteudo")
-    let div = this.CriarDivInputDetalhes();
-    div.appendChild(CriarTextArea());
-    AreaDeConteudo?.appendChild(div);
-   
-    function CriarTextArea():Node {
-      let TextArea = document.createElement("textarea");
-      TextArea.setAttribute("class","form-control");
-      TextArea.setAttribute("id","textarea-conteudo")
-      TextArea.setAttribute("name","imagem");
-      TextArea.setAttribute("formControlName","texto");
-      TextArea.setAttribute("style","height: 200px;");
-      return TextArea;
-    }
-  }
-  
-  CriarComponenteInputImagem(){
-    let AreaDeConteudo = document.getElementById("area-de-conteudo")
-    let div = this.CriarDivInputImagem();
-    let Imagem = document.createElement("input");
-    Imagem.setAttribute("type","file");
-    Imagem.setAttribute("class","form-control");
-    Imagem.setAttribute("name","imagem");
-    Imagem.setAttribute("formControlName","imagem");
-    Imagem.setAttribute("accept","image/png, image/jpeg"  )
-    Imagem.setAttribute("change","TratarArquivoImagem($event)");
-    Imagem.addEventListener("change",this.TratarArquivoImagem)
-    div.appendChild(Imagem);
-    AreaDeConteudo?.appendChild(div);
-    Imagem.click(); 
-  }
-
-  TratarArquivoImagem(event: any){
-    let IdElementeEmFoco = event.target.parentNode.id;
-    let IdNovoElemento = String(document.getElementsByTagName("img").length+1);
-    let DivMiniaturaImagem = document.getElementById("div-miniatura-img"+IdElementeEmFoco)
-    
-    if(DivMiniaturaImagem){
-       DivMiniaturaImagem!.remove();
-      let AreaDeConteudoExistente = document.getElementById(IdElementeEmFoco)
-      AreaDeConteudoExistente?.appendChild(CriarDivCardImagem(IdElementeEmFoco))
-    }
-    else{
-      let AreaDeConteudoNovo = document.getElementById(IdNovoElemento)
-      AreaDeConteudoNovo?.appendChild(CriarDivCardImagem(IdNovoElemento))
-    }
-    function CriarDivCardImagem(posicaoId:string):Node{
-      let  DivQuadroInformacao = document.createElement("div");
-      DivQuadroInformacao.setAttribute("class","desc-input");
-      DivQuadroInformacao.setAttribute("id","div-miniatura-img"+posicaoId);
-      DivQuadroInformacao?.appendChild(CriarMiniaturaImagem(event));
-      DivQuadroInformacao.appendChild(CriarDivDetalhesImagem());
-      return DivQuadroInformacao;
-    }
-
-    function CriarMiniaturaImagem(event: any):Node{
-      let imagem = document.createElement("img");
-      imagem.setAttribute("class","desc-input-img");
-      imagem.src = URL.createObjectURL(event.target.files[0]);
-      return imagem;
-    }
-
-    function CriarDivDetalhesImagem(){
-      let  DivDescricaoImagem = document.createElement("div");
-      DivDescricaoImagem.setAttribute("class","desc-input-img-details border");
-      DivDescricaoImagem.appendChild(CriarListDescricaoImagem()!);
-      return DivDescricaoImagem;
-    }
-    function CriarListDescricaoImagem(){
-      let ul = document.createElement("ul")
-      ul.setAttribute("style","list-style: none; margin-bottom: 0px");
-      let TamanhoImagem = document.createElement("li");
-      let UltimaModific = document.createElement("li");
-      TamanhoImagem.textContent = `Tamanho: ${event.target.files[0].size} bytes`
-      UltimaModific.textContent = `Ultima Modificação:${event.target.files[0].lastModifiedDate}`
-
-      ul.appendChild(TamanhoImagem)
-      ul.appendChild(UltimaModific)
-      return ul;
-    }
-  }
-  
   CreateInputeTextLinkYoutube(){
-    let AreaDeConteudo = document.getElementById("area-de-conteudo")
-    let TexYoutube = document.createElement("input");
-    let div = this.CriarDivInputDetalhes();
-    
-    TexYoutube.setAttribute("class","form-control");
-    TexYoutube.setAttribute("type","text")
-    TexYoutube.setAttribute("name","idvideoyoutube");
-    TexYoutube.setAttribute("formControlName","idvideoyoutube");
-    TexYoutube.addEventListener("blur",this.ObterIdVideo);
-    
-    div.appendChild(TexYoutube)
-    AreaDeConteudo?.appendChild(div);
+    this.HTMLDinamico.CreateInputeTextLinkYoutube();
   }
-
-  ObterIdVideo(e:any){
-    e.target.value = String(e.target.value).replace("https://www.youtube.com/watch?v=",'');
-  }
-
+  
   MapearInputImagemVazioFormGroup(){
     this.conteudoDatalhesForm.push(
       this.fb.group({
@@ -194,7 +85,7 @@ export class FormConteudoComponent implements OnInit,AfterViewInit{
   MapearTextAreaTextoVazioFormGroup(){
     this.conteudoDatalhesForm.push(
       this.fb.group({
-        texto: ['']
+        texto: []
       })
     );
   }

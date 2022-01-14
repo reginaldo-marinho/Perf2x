6e70aec254e6488d0e29e94dea57b277938ca2be
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-01-13 22:54:53 -0300

    commit

---
+++ src/app/Clientes/clientes.component.ts
@@ -0,0 +1,14 @@
+
+
+
+
+
+import {  Component} from "@angular/core";
+@Component({
+    templateUrl:'./clientes.component.html',
+    styleUrls:['./clientes.component.css']
+})
+export class ClienteComponent 
+{
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ListConteudoComponent } from './list-conteudo/list-conteudo.component';
 import { MenuBrazukasComponent } from './core/menu-conteudo/menu-brazukas.component';
 import { Page404Component } from './core/page-404/page-404.component';
 import { TemaEnsinoComponent } from './Tema-ensino/tema-ensino.component';
+import { ClienteComponent } from './Clientes/clientes.component';
 
 const routes: Routes = [
   {path:'menu-brazukas', component: MenuBrazukasComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:'Form-conteudo/:codconteudo', component: FormConteudoComponent},
   {path:'ulti-posts'   , component: ListConteudoComponent},
   {path:'tema-ensino'   , component: TemaEnsinoComponent},
+  {path:'clientes'   , component: ClienteComponent},
   {path:'',redirectTo:'menu-brazukas', pathMatch: 'full'},
   {path:'**', component:Page404Component}
 ];
